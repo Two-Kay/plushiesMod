30e9e0d5bbd6a433fbc2f0f44546bef4d1c5591a
Toggle <toggledy@gmail.com>
2020-10-14 05:39:00 +1100

    Cleanup

--- Source/Plushies/HarmonyPatches.cs
+++ Source/Plushies/HarmonyPatches.cs
@@ -29,7 +29,6 @@ namespace Plushies
     [StaticConstructorOnStartup]
     static class HarmonyPatches
     {
-        // this static constructor runs to create a HarmonyInstance and install a patch.
         static HarmonyPatches()
         {
             var harmony = new Harmony("rimworld.cute.plushies");
@@ -41,27 +40,13 @@ namespace Plushies
     [HarmonyPatch("ApplyBedThoughts")]
     class BedThoughtsPatch
     {
-    // public IEnumerable<Building_Bed> ContainedBeds
-    // {
-    //   get
-    //   {
-    //     List<Thing> things = this.ContainedAndAdjacentThings;
-    //     for (int i = 0; i < things.Count; ++i)
-    //     {
-    //       if (things[i] is Building_Bed buildingBed)
-    //         yield return buildingBed;
-    //     }
-    //   }
-    // }
         static void Postfix(Pawn actor)
         {
             if (actor.needs.mood == null)
                 return;
 
-            Log.Message("Plushies mod saw ApplyBedThoughts run!");
             var roomThings = actor.GetRoom().ContainedAndAdjacentThings;
             var hasPlushie = roomThings.Any(thing => thing.def.defName == "Plushie");
-            Log.Message($"Room has plushie? {hasPlushie}");
             if (hasPlushie) {
                 actor.needs.mood.thoughts.memories.TryGainMemory(PlushiesThoughtDefOf.PlushieCuddle);
             }
