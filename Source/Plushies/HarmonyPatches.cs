c2324d9023f32bd4ef7a8189485d13fbaa9db3bf
Toggle <toggledy@gmail.com>
2020-10-14 05:38:13 +1100

    Sleeping with plushie in room causes happy thought

--- Source/Plushies/HarmonyPatches.cs
+++ Source/Plushies/HarmonyPatches.cs
@@ -4,6 +4,28 @@ using Verse;
 
 namespace Plushies
 {
+    [DefOf]
+    public static class PlushiesThingDefOf
+    {
+        public static ThingDef Plushie;
+
+        static PlushiesThingDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(PlushiesThingDefOf));
+        }
+    }
+
+    [DefOf]
+    public static class PlushiesThoughtDefOf
+    {
+        public static ThoughtDef PlushieCuddle;
+
+        static PlushiesThoughtDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(PlushiesThoughtDefOf));
+        }
+    }
+
     [StaticConstructorOnStartup]
     static class HarmonyPatches
     {
@@ -19,9 +41,30 @@ namespace Plushies
     [HarmonyPatch("ApplyBedThoughts")]
     class BedThoughtsPatch
     {
-        static void Postfix()
+    // public IEnumerable<Building_Bed> ContainedBeds
+    // {
+    //   get
+    //   {
+    //     List<Thing> things = this.ContainedAndAdjacentThings;
+    //     for (int i = 0; i < things.Count; ++i)
+    //     {
+    //       if (things[i] is Building_Bed buildingBed)
+    //         yield return buildingBed;
+    //     }
+    //   }
+    // }
+        static void Postfix(Pawn actor)
         {
+            if (actor.needs.mood == null)
+                return;
+
             Log.Message("Plushies mod saw ApplyBedThoughts run!");
+            var roomThings = actor.GetRoom().ContainedAndAdjacentThings;
+            var hasPlushie = roomThings.Any(thing => thing.def.defName == "Plushie");
+            Log.Message($"Room has plushie? {hasPlushie}");
+            if (hasPlushie) {
+                actor.needs.mood.thoughts.memories.TryGainMemory(PlushiesThoughtDefOf.PlushieCuddle);
+            }
         }
     }
 }
